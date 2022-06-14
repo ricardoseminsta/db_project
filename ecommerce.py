b500c1e9af9c5f255ff7ee86003da708d79f9438
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-06-14 17:00:07 -0300

    commit set order complete

--- ecommerce.py
+++ ecommerce.py
@@ -1,3 +1,4 @@
+from multiprocessing import Condition
 import sqlite3
 from tabulate import tabulate
 from datetime import datetime
@@ -48,13 +49,36 @@ def set_purchase():
     data_atual = datetime.now()
     data_texto = data_atual.strftime("%Y-%m-%d")
     list_costumers()
-    id = int(input("\nDigite o id do Cliente: "))
-    cursor.execute(f'INSERT INTO purchase (id_purchase, id_costumer, date_purchase) VALUES (null, ?, ?);', (id, data_texto))
+    print("\nEscolha um cliente pelo nuemro do cpf")
+    cpf = int(input("\nDigite o cpf do Cliente (apenas numeros): "))
+    cursor.execute(f'SELECT id_costumer FROM costumer WHERE cpf = {cpf};')
+    id = cursor.fetchone()
+    cursor.execute(f'INSERT INTO purchase (id_purchase, id_costumer, date_purchase) VALUES (null, ?, ?);', (id[0], data_texto))
     connection.commit()
-    cursor.execute("SELECT id_purchase from purchase ORDER BY id_purchase DESC LIMIT 1")
-    last_purchase = cursor.fetchone();
-    print("Selecione o produto")
-    list_products()
-    
+
+def set_order():
+    set_purchase()
+
+    out = False
+    num = 0
+    prods = []
+    while out is False:
+        if num == 0:
+            list_products()
+            id_product = int(input("\nDigite o id do produto: "))
+            amount = int(input("\nDigite a quantidade: "))
+            cursor.execute("SELECT id_purchase from purchase ORDER BY id_purchase DESC LIMIT 1;")
+            last_purchase = cursor.fetchone();
+
+            data = (id_product, last_purchase[0], amount)
+            prods.append(data)
+
+            num = int(input("\nDigite 0 para continuar adicionando itens: "))
+        else:
+            cursor.executemany(f'INSERT INTO product_purchase (id_product_purchase, id_product, id_purchase, product_amount) VALUES (null, ?, ?, ?);', prods)
+            connection.commit()
+            out = True
+
+set_order()
 
 connection.close()
